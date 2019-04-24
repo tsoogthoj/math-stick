9eb37c052162eb56234d0a47575033269efb60c9
Chong Thao <tsoogthoj@hotmail.com>
2019-04-24 09:30:53 -0700

    modal function and added more button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Box from "./components/mathBox/box";
 
 class App extends Component {
+  state = {
+    modalOpen: false,
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,8 +17,14 @@ class App extends Component {
         <div className="printButton button">
           Print
         </div>
+        <div className="alphabetButton button">
+          Alphabets Key
+        </div>
       </div>
         < Box />
+        <div className={"modal " + (this.state.modalOpen ? "modalOpen" : "")}>
+        
+        </div>
       </div>
     );
   }
---
+++ src/components/alphabet/alphabet.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Alphabet extends Component {
+    render() {
+        return(
+            <div className="alphabet-wrapper">
+            
+            </div>
+
+        )
+    }
+}
+
+export default Alphabet
