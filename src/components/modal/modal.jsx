6d7283c367d18421915f8b3e5621a89770a53d26
Chong Thao <tsoogthoj@hotmail.com>
2019-04-25 09:56:37 -0700

    modal addition - clear icon, modal body

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react"
 import "./modal.css"
+import clearIcon from "./img/clear.svg"
+
 class Modal extends Component {
 
     render() {
         return (
             <div className="modal-wrapper">
+                <div className="modal-exit">
+                    <img src={clearIcon} alt="X"/>
+                </div>
+                <div className="modal-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis magnam nisi animi ullam, minus reiciendis veniam atque itaque minima, vel neque. Placeat molestiae laudantium eveniet voluptatum iste optio adipisci nesciunt!
+                </div>
             </div>
         )
     }
