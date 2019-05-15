494f8e98971fdeff6a91b069c8b824af53401d16
Chong Thao <tsoogthoj@hotmail.com>
2019-05-15 15:17:53 -0700

    dynamic word form

--- src/components/word/word.jsx
+++ src/components/word/word.jsx
@@ -2,12 +2,27 @@ import React, { Component } from "react"
 import "./word.css"
 
 class Word extends Component {
+    state = {
+        wordAmount: 1,
+    }
+    
     handleSubmit(event) {
         event.preventDefault()
         console.log("sumbit")
-      }
+    }
 
+    addWord = () => {
+        this.state.wordAmount += 1
+        
+    }
+
+    wordForm = () => {
+
+    }
+    
     render() {
+        
+        
         return (
             <div className="word-wrapper">
                 <form className="word-form" onSubmit={this.handleSubmit}>
@@ -17,11 +32,12 @@ class Word extends Component {
                         </label>
                         <input type="text" />
                     </div>
-                    
-                    <div className="wordAdd">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                    <div className="word-addSubmit">
+                        <div className="wordAdd" onClick={this.addWord}>
+                            <svg width="40" height="40" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                        </div>
+                        <input className="wordSubmit" type="submit" value="Submit" />
                     </div>
-                    <input className="wordSubmit" type="submit" value="Submit" />
                 </form>
             </div>
         )
