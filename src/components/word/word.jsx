d4dc25f63860687b5eb970ff0e6dfaf15542061a
Chong Thao <tsoogthoj@hotmail.com>
2019-05-17 09:21:24 -0700

    map word worm

--- src/components/word/word.jsx
+++ src/components/word/word.jsx
@@ -4,37 +4,50 @@ import "./word.css"
 class Word extends Component {
     state = {
         wordAmount: 1,
+        wordArray: [1],
     }
-    
+
     handleSubmit(event) {
         event.preventDefault()
         console.log("sumbit")
     }
 
     addWord = () => {
-        this.state.wordAmount += 1
-        
+        let addNum = this.state.wordAmount
+        addNum += 1
+        let wordArray = this.state.wordArray
+        wordArray.push(addNum)
+        console.log(wordArray)
+        this.setState({
+            wordAmount: addNum,
+            wordArray: wordArray
+        }, () => {
+            console.log(this.state)
+        })
+
     }
 
     wordForm = () => {
 
     }
-    
+
+
     render() {
-        
-        
+
         return (
             <div className="word-wrapper">
                 <form className="word-form" onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label className="wordLabel">
-                            Word 1
-                        </label>
-                        <input type="text" />
-                    </div>
+                    {this.state.wordArray.map( num => 
+                        <div className="form-group" key={num}>
+                            <label className="wordLabel">
+                                Word {num}
+                            </label>
+                            <input type="text" />
+                        </div>
+                    )}
                     <div className="word-addSubmit">
                         <div className="wordAdd" onClick={this.addWord}>
-                            <svg width="40" height="40" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                            <svg width="40" height="40" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /><path d="M0 0h24v24H0z" fill="none" /></svg>
                         </div>
                         <input className="wordSubmit" type="submit" value="Submit" />
                     </div>
