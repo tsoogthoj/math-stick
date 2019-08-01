50db35b714959bdff2399e21c62b86f671bf525a
Chong Thao <tsoogthoj@hotmail.com>
2019-07-31 17:19:38 -0700

    Reset app.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,11 @@
-import React, { Component } from 'react'
-import './App.css'
-import Box from "./components/mathBox/box"
-import Modal from "./components/modal/modal"
+import React from 'react';
 
-class App extends Component {
-  state = {
-    modalOpen: true,
-    openedModal: "add",
-  }
+function App() {
+  return (
+    <div>
 
-  modalStatus = () => {
-    this.state.modalOpen ? (this.setState({ modalOpen: false })) : (this.setState({ modalOpen: true }))
-  }
-
-  openedModal = (event) => {
-    this.setState({
-      openedModal: event.target.dataset.name
-    })
-    console.log(event.target.dataset.name)
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="header">
-          <div className="addMoreButton button"
-            data-name="add"
-            onClick={ (event) => {this.modalStatus(); this.openedModal(event)}
-            }
-          >
-            Add Words/Sentence
-        </div>
-          <div className="printButton button"
-            onClick={this.modalStatus}>
-            Print
-        </div>
-          <div className="alphabetButton button"
-            data-name="key"
-            onClick={ (event) => {this.modalStatus(); this.openedModal(event)}
-          }>
-            Alphabets Key
-        </div>
-        </div>
-        < Box />
-        <div className={(this.state.modalOpen ? "modalOpen" : "modalClose")}>
-          {(this.state.modalOpen ?
-            < Modal
-              active={this.modalStatus}
-              openedModal={this.state.openedModal}
-            /> : null)}
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
