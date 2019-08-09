d56e0760f61d1cca14272aecb6fbf76a64ae8790
Chong Thao <tsoogthoj@hotmail.com>
2019-08-09 09:05:14 -0700

    folder sturctures

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import React from 'react';
+git import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout.jsx'
+import Math from './containers/Math/Math'
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  state = {
+    words: [],
+    mathkey: {
+      A: 7,
+      B: 5,
+      C: 9,
+      D: 1,
+      E: 12,
+      F: 45,
+      G: 43,
+      H: 87,
+      I: 12,
+      J: 42,
+      K: 63,
+      L: 21,
+      M: 7,
+      N: 8,
+      O: 12,
+      P: 32,
+      R: 87,
+      S: 45,
+      T: 12,
+      U: 12,
+      V: 56,
+      W: 72,
+      X: 53,
+      Y: 12,
+      Z: 45
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <Layout>
+            <Math mathKey={this.state.Mathkey}/>
+        </Layout>
+      </div>
+    );
+
+  }
 }
+
 
 export default App;
---
+++ src/containers/Math/Math.jsx
@@ -0,0 +1,18 @@
+import React, { Component }from 'react'
+
+class Math extends Component {
+    state = {
+        letters: ['apple']
+    }
+
+    render() {
+
+        return(
+            <div>
+                Math Component
+            </div>
+        ) 
+    }
+}
+
+export default Math
